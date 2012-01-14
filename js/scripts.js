12b7e8067acaeab10143f303da5f031fbb2e38e1
Andrews F.G <andrewsfg@gmail.com>
2012-01-13 22:02:16 -0200

    media querie for ipad tests

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@ $('#main ul li a').click(function(){
 $('.back').click(function(){
   $("#main").removeClass("effect").removeAttr("class");
 });
+
+//ipad and iphone fix
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+    $(".flip").click(function(){
+        //we just need to attach a click event listener to provoke iPhone/iPod/iPad's hover event
+        //strange
+    });
+}
