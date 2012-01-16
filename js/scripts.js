5c0312c5d286788321f90cb580067bd73818bcf6
Andrews F.G <andrewsfg@gmail.com>
2012-01-16 19:47:30 -0200

    ipad effects hardware acceleration

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,23 @@ $('.back').click(function(){
   $("#main").removeClass("effect").removeAttr("class");
 });
 
-//ipad and iphone fix
-if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
-    $(".flip").click(function(){
-        //we just need to attach a click event listener to provoke iPhone/iPod/iPad's hover event
-        //strange
-    });
-}
+if (document.body.ontouchstart === undefined){ // this is for all browsers
+      $(document).click(function(event){
+          var x= event.offsetX;
+          if(!x){
+            x= event.layerX ;
+          }
+          var y= event.offsetY;
+          if(!y){
+            y= event.layerY ;
+          }
+       //alert("x:"+x+"y:"+y);
+      });
+  }else {  // this is for all touch devices
+      $(document).bind('touchstart',function(event){
+          var orig = event.originalEvent;  
+          var x = orig.changedTouches[0].pageX;  
+          var y = orig.changedTouches[0].pageY;  
+          //alert("x:"+x+"y:"+y);
+      });
+  }
